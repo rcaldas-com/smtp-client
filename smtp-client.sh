d4427413b4821237fdce9eb931b31b295bceac7e
RCaldas UNB <unb@rcaldas.com>
2019-08-27 16:30:05 -0300

    Init file

---
+++ smtp-client.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+### SMTP client for command line ### @RCaldas84
+
+### Load settings from .env file or uncomment next:
+source .env
+# SMTP_SERVER='server.domain:587'
+# SMTP_USER='smtpuser'
+# SMTP_PWD='userpassword'
+# SMTP_TLS=1
+# DOMAIN='domain.com'
+# ADMIN_MAIL='your@mail.com'
