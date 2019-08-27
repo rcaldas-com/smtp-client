ef9a96f9a4125771e318ed001e9da55f402891d9
RCaldas UnB <robson@rcaldas.com>
2019-08-27 20:44:45 -0300

    Add postfix config

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
-# smtp-client
-Setting Linux as a SMTP client using external server for command line email sending.
+# postfix-smtp-client
+Setting postfix as a SMTP client (relay) using external server for command line and system mail sending.
 
-To send mail from mutt, crontab, sendmail, mail, mailx...
-
-Some choices of agents are:
-- exim4
-- postfix
-- msmtp
+To send mail from mutt, crontab, sendmail, mail...
--- smtp-client.sh
+++ smtp-client.sh
@@ -3,28 +3,47 @@
 
 ### Load settings from .env file or uncomment next:
 source .env
-# SMTP_SERVER='server.domain:587'
-# SMTP_USER='smtpuser'
+# SMTP_SERVER='server.domain'
+# SMTP_PORT=587
+# SMTP_USER='user@mail.com'
 # SMTP_PWD='userpassword'
-# SMTP_TLS=1
 # DOMAIN='domain.com'
 # ADMIN_MAIL='your@mail.com'
 
 ### Set /etc/mailname
-sudo echo "$DOMAIN" > /etc/mailname
+echo "$DOMAIN" | sudo tee /etc/mailname >/dev/null
 
-### /etc/mail.rc
+### Check fot postfix package
+if ! dpkg -s postfix &> /dev/null; then
+  sudo apt update
+  sudo apt install -y postfix
+fi
 
+### Set sasl_passwd file
+echo "[$SMTP_SERVER]:$SMTP_PORT $SMTP_USER:$SMTP_PWD" | \
+  sudo tee /etc/postfix/sasl_passwd >/dev/null
+sudo chmod 600 /etc/postfix/sasl_passwd
+sudo postmap /etc/postfix/sasl_passwd
 
-### ~/.mailrc
-
+### Set main.cf file
+[ ! -e /etc/postfix/main.cf.bkp ] && \
+  sudo cp /etc/postfix/main.cf /etc/postfix/main.cf.bkp
+cat <<EOF | sudo tee /etc/postfix/main.cf >/dev/null
+myhostname = $DOMAIN
+inet_interfaces = loopback-only
+relayhost = [$SMTP_SERVER]:$SMTP_PORT
+smtp_sasl_auth_enable = yes
+smtp_sasl_security_options = noanonymous
+smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd
+smtp_use_tls = yes
+smtp_tls_CAfile = /etc/ssl/certs/ca-certificates.crt
+mynetworks_style = host
+EOF
 
 ### Set MAILTO in crontab, replace if exist or add at top:
 grep -q 'MAILTO=' /etc/crontab && \
   sudo sed -i "/MAILTO=/c\MAILTO=$ADMIN_MAIL" /etc/crontab || \
   sudo sed -i "1iMAILTO=$ADMIN_MAIL" /etc/crontab
 
-
-### With exim4
-### With postfix
-### With msmtp
+### Restart postfix
+sudo systemctl restart postfix
